2e0722d95ea73e7f7ee6da6ec6718b25756f65dc
reddymanoj@gmail.com <reddymanoj@yahoo.com>
2012-02-02 11:34:05 -0600

    branching out to fill in the layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title){"Ruby on Rails Tutorial Sample App"}
   
   describe "Home Page" do
 
-    it "should have sample content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1',:text => 'Sample App')
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                                :text=>"Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text=>"#{base_title} | Home")
     end
   end
 
@@ -23,7 +23,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text=>"Ruby on Rails Tutorial Sample App | Help")
+                                :text=>"#{base_title} | Help")
     end
   end
 
@@ -35,7 +35,15 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text=>"Ruby on Rails Tutorial Sample App | About Us")
+                                :text=>"#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                :text=>"#{base_title} | Contact")
     end
   end
 end
